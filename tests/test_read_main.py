42acd0ad8c14e73545aae5dc5a65e1b655dc7522
Abhinav1190P <pandeysandeep1190@gmail.com>
2023-10-31 21:15:56 +0530

    Added 2 more tests

--- tests/test_read_main.py
+++ tests/test_read_main.py
@@ -7,3 +7,17 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+
+def test_read_root_content():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "Hello" in response.json().keys()
+    assert response.json()["Hello"] == "World"
+
+
+
+def test_read_nonexistent_endpoint():
+    response = client.get("/nonexistent")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Not Found"}
